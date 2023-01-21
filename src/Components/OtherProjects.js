cce8e3986a568c7abcc0b3f64ea393e8be95d0c4
Lesley <lesleyhouu@gmail.com>
2023-01-21 12:54:04 -0500

    Add: window resizing functionality for Projects.js

--- src/Components/OtherProjects.js
+++ src/Components/OtherProjects.js
@@ -18,7 +18,7 @@ const OtherProjects = () => {
                     <span className="project-label">App Development</span>
                 </div>
                 <p className="project-description-long">An app that matches you with like-minded partners to keep you motivated, accountable, and on track for success. For SheHacks+ 7, 2023. </p>
-                <button className="project-show-detail-button">Details soon <img src={lock} width="18"/></button>
+                <button className="project-show-detail-button">Details soon ‎<img src={lock} width="18"/></button>
             </div>
             <div className="vertical-divider"></div>
             <div className="project-div">
@@ -30,7 +30,7 @@ const OtherProjects = () => {
                     <span className="project-label">Project</span>
                 </div>
                 <p className="project-description-long">Extended functionality for Learn including navigation and UI improvements to Room Bookings page. See it in action (soon).</p>
-                <button className="project-show-detail-button">Details soon <img src={lock} width="18"/></button>
+                <button className="project-show-detail-button">Details soon ‎<img src={lock} width="18"/></button>
             </div>
             <div className="vertical-divider"></div>
             <div className="project-div">
@@ -46,7 +46,7 @@ const OtherProjects = () => {
                     <span className="project-label">Web Application</span>
                 </div>
                 <p className="project-description-long">An web application that identifies bird species from an image or a live video feed. For Western AI’s Projects team.</p>
-                <button className="project-show-detail-button">Details soon <img src={lock} width="18"/></button>
+                <button className="project-show-detail-button">Details soon ‎<img src={lock} width="18"/></button>
             </div>
             
         </div>
